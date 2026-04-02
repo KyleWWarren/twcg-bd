0f54b7185188e481e4b6763394ea815c84a09a94
Kyle W. Warren <Kyle@lighthouseresearchgroup.net>
2026-04-02 13:12:54 +0000

    Override lint task

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,7 @@ const build = require('@microsoft/sp-build-web');
 build.sass.setConfig({ tryToUseNpmModule: true });
 build.sass.enabled = false;
 build.addSuppression(/Warning - \[sass\]/gi);
+build.addSuppression(/Warning - \[lint\]/gi);
+const buildLint = build.subTask('lint', function(gulp, buildConfig, done) { done(); });
+build.task('lint', buildLint);
 build.initialize(require('gulp'));
